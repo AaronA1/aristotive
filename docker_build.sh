d3ba1ffcf1edc57ac351ea73bda652d0f930d9b1
Aaron <adejumoa@aston.ac.uk>
2020-11-12 03:35:16 +0000

    Dockerised
    App dockerised on LEMP stack

---
+++ docker_build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# copy env
+FILE='.env'
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+else
+   echo "File $FILE does not exist, copying .env.example."
+   cp .env.example .env
+fi
+
+# build container & install packages
+docker-compose up --build --remove-orphans -d
+docker-compose exec aristotive-app composer install
+docker-compose exec aristotive-app php artisan key:generate
+
+# npm
+docker-compose run aristotive-node npm install
+docker-compose run aristotive-node npm audit fix
+docker-compose run aristotive-node npm run dev
+
+echo "Open the app: http://localhost:8084"
