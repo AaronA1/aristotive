92bfa069b41a663734d953acdf4e805a5a86b50f
Aaron <adejumoa@aston.ac.uk>
2021-10-12 15:09:46 +0100

    Use Laravel Sail

--- docker_build.sh
+++ docker_build.sh
@@ -10,17 +10,16 @@ else
 fi
 
 # build container & install packages
-docker-compose up --build --remove-orphans -d
-docker-compose exec aristotive-app composer install
-docker-compose exec aristotive-app php artisan key:generate
-docker-compose exec aristotive-app php artisan migrate:fresh --seed
+./vendor/bin/sail up -d
+docker exec -it aristotive-app-1 composer install
+docker exec -it aristotive-app-1 php artisan key:generate
+docker exec -it aristotive-app-1 php artisan migrate:fresh --seed
 
-# npm
-docker-compose run aristotive-node npm install
-docker-compose run aristotive-node npm run dev
+docker exec -it aristotive-app-1 npm install
+docker exec -it aristotive-app-1 npm run dev
 
-echo "Open the app: http://localhost:8084"
+echo "Open the app: http://0.0.0.0"
 IP=$(ipconfig getifaddr en0)
-echo "Students can connect via this address: ${IP}:8084"
+echo "Access on the same LAN with this IP & Port: ${IP}:80"
 
 
